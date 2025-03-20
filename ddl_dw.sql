450429a69b555b3462708a9e2a4d64777056f2ea
azislhn <azizslhn1105@gmail.com>
2025-03-20 12:33:00 +0700

    Update DDL

--- ddl_dw.sql
+++ ddl_dw.sql
@@ -1,43 +1,40 @@
 -- DDL Script untuk Fact Constellation Schema - Brazilian E-Commerce Public Dataset
 
 -- Tabel Dimensi
-CREATE OR REPLACE TABLE dim_geolocation (
-    geolocation_sk INT PRIMARY KEY,
-    zip_code_prefix STRING UNIQUE NOT NULL,
-    latitude DECIMAL(10,6) NOT NULL,
-    longitude DECIMAL(10,6) NOT NULL,
-    geolocation_city STRING NOT NULL,
-    geolocation_state STRING NOT NULL
+CREATE OR REPLACE TABLE dim_location (
+    zip_code_prefix INTEGER PRIMARY KEY,
+    city_name VARCHAR NOT NULL,
+    state_abbr VARCHAR NOT NULL
 );
 
 CREATE OR REPLACE TABLE dim_customers (
-    customer_sk INT PRIMARY KEY,
-    customer_id STRING UNIQUE NOT NULL,
-    customer_unique_id STRING NOT NULL,
-    geolocation_sk INT NOT NULL,
-    FOREIGN KEY (geolocation_sk) REFERENCES dim_geo(geolocation_sk)
+    customer_key INTEGER PRIMARY KEY,
+    customer_id VARCHAR UNIQUE NOT NULL,
+    customer_unique_id VARCHAR NOT NULL,
+    customer_zip_code_prefix INTEGER,
+    FOREIGN KEY (customer_zip_code_prefix) REFERENCES dim_location(zip_code_prefix)
 );
 
 CREATE OR REPLACE TABLE dim_sellers (
-    seller_sk INT PRIMARY KEY,
-    seller_id STRING UNIQUE NOT NULL,
-    geolocation_sk INT NOT NULL,
-    FOREIGN KEY (geolocation_sk) REFERENCES dim_geo(geolocation_sk)
+    seller_key INTEGER PRIMARY KEY,
+    seller_id VARCHAR UNIQUE NOT NULL,
+    seller_zip_code_prefix INTEGER,
+    FOREIGN KEY (seller_zip_code_prefix) REFERENCES dim_location(zip_code_prefix)
 );
 
 CREATE OR REPLACE TABLE dim_time (
-    time_sk INT PRIMARY KEY,
-    date DATE UNIQUE NOT NULL,
-    year INT NOT NULL,
-    month INT NOT NULL,
-    day_of_week STRING NOT NULL
+    time_key INTEGER PRIMARY KEY,
+    time_date DATE UNIQUE NOT NULL,
+    time_year INTEGER NOT NULL,
+    time_month INTEGER NOT NULL,
+    time_day_of_week VARCHAR NOT NULL
 );
 
 -- SCD Type 2 untuk Dimensi Products
 CREATE OR REPLACE TABLE dim_products (
-    product_sk INT PRIMARY KEY,
-    product_id STRING NOT NULL,
-    product_category_name STRING NOT NULL,
+    product_key INTEGER PRIMARY KEY,
+    product_id VARCHAR NOT NULL,
+    product_category_name VARCHAR NOT NULL,
     product_weight_g FLOAT NOT NULL,
     product_length_cm FLOAT NOT NULL,
     start_date TIMESTAMP NOT NULL,
@@ -47,40 +44,40 @@ CREATE OR REPLACE TABLE dim_products (
 
 -- Tabel Fakta
 CREATE OR REPLACE TABLE fact_orders (
-    order_id STRING PRIMARY KEY,
-    customer_sk INT NOT NULL,
-    order_status STRING NOT NULL,
+    order_id VARCHAR PRIMARY KEY,
+    customer_key INTEGER NOT NULL,
+    order_status VARCHAR NOT NULL,
     order_purchase_timestamp TIMESTAMP NOT NULL,
     order_estimated_delivery_date DATE NOT NULL,
-    FOREIGN KEY (customer_sk) REFERENCES dim_customers(customer_sk)
+    FOREIGN KEY (customer_key) REFERENCES dim_customers(customer_key)
 );
 
 CREATE OR REPLACE TABLE fact_order_items (
-    order_id STRING NOT NULL,
-    order_item_id INT NOT NULL,
-    product_sk INT NOT NULL,
-    seller_sk INT NOT NULL,
+    order_id VARCHAR NOT NULL,
+    order_item_id INTEGER NOT NULL,
+    product_key INTEGER NOT NULL,
+    seller_key INTEGER NOT NULL,
     price FLOAT NOT NULL,
     freight_value FLOAT NOT NULL,
     PRIMARY KEY (order_id, order_item_id),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id),
-    FOREIGN KEY (product_sk) REFERENCES dim_products_scd(product_sk),
-    FOREIGN KEY (seller_sk) REFERENCES dim_sellers(seller_sk)
+    FOREIGN KEY (product_key) REFERENCES dim_products(product_key),
+    FOREIGN KEY (seller_key) REFERENCES dim_sellers(seller_key)
 );
 
 CREATE OR REPLACE TABLE fact_payments (
-    order_id STRING NOT NULL,
-    payment_sequential INT NOT NULL,
-    payment_type STRING NOT NULL,
+    order_id VARCHAR NOT NULL,
+    payment_sequential INTEGER NOT NULL,
+    payment_type VARCHAR NOT NULL,
     payment_value FLOAT NOT NULL,
     PRIMARY KEY (order_id, payment_sequential),
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id)
 );
 
 CREATE OR REPLACE TABLE fact_reviews (
-    review_id STRING PRIMARY KEY,
-    order_id STRING NOT NULL,
-    review_score INT NOT NULL,
+    review_id VARCHAR PRIMARY KEY,
+    order_id VARCHAR NOT NULL,
+    review_score INTEGER NOT NULL,
     review_creation_date DATE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES fact_orders(order_id)
 );
